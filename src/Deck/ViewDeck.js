de017a282c273480a6bcecc36692106d1b4a44e0
Thinh Luu <thinhpluu96@gmail.com>
2022-07-22 12:21:23 -0700

    edit deck workds

--- src/Deck/ViewDeck.js
+++ src/Deck/ViewDeck.js
@@ -3,25 +3,42 @@ import { useParams, useRouteMatch, useHistory } from "react-router-dom";
 import { useState, useEffect } from "react";
 import CardsList from "../Card/CardsList";
 
-export default function ViewDeck({ cards, decks, deck }) {
+export default function ViewDeck({ cards }) {
+	const [allDecks, setAllDecks] = useState([]);
+	const [currentDeck, setCurrentDeck] = useState([]);
 	const { deckId } = useParams("deckId");
 	const { url } = useRouteMatch();
 	const history = useHistory();
 	const allCards = JSON.parse(localStorage.getItem("cards")) || [];
 
+	useEffect(() => {
+		let decksFromStorage = localStorage.getItem("decks");
+		if (decksFromStorage) {
+			decksFromStorage = JSON.parse(decksFromStorage);
+			setAllDecks(decksFromStorage);
+			setCurrentDeck(
+				decksFromStorage.find((deck) => deck.id === Number(deckId))
+			);
+		}
+	}, []);
+
 	function handleDeleteDeck() {
 		if (
 			window.confirm("Do you want to delete this deck? It cannot be recovered.")
 		) {
-			const newDecks = decks.filter((oldDeck) => oldDeck.id !== deck.id);
-			const newCardsList = allCards.filter((card) => card.deckId !== deck.id);
+			const newDecks = allDecks.filter(
+				(oldDeck) => oldDeck.id !== Number(deckId)
+			);
+			const newCardsList = allCards.filter(
+				(card) => card.deckId !== Number(deckId)
+			);
 			localStorage.setItem("decks", JSON.stringify(newDecks));
 			localStorage.setItem("cards", JSON.stringify(newCardsList));
 			history.push("/");
 		}
 	}
-	console.log(deck.title, deck.desc, "from view");
-	return deck ? (
+	console.log(currentDeck.title, currentDeck.desc, "from view");
+	return currentDeck ? (
 		<div className='container'>
 			{/* Bread crumb nav bar */}
 			<nav aria-label='breadcrumb'>
@@ -30,12 +47,12 @@ export default function ViewDeck({ cards, decks, deck }) {
 						<a href='/'>Home</a>
 					</li>
 					<li className='breadcrumb-item active' aria-current='page'>
-						{deck.title}
+						{currentDeck.title}
 					</li>
 				</ol>
 			</nav>
-			<h5>{deck.title}</h5>
-			<p>{deck.desc}</p>
+			<h5>{currentDeck.title}</h5>
+			<p>{currentDeck.desc}</p>
 			<section className='deck-operation-links'>
 				<a href={`${url}/edit`} className='btn btn-info mr-2'>
 					Edit
