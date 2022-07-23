e7eecbc7fd2c57f82a1716113c8157379f286cb1
Thinh Luu <thinhpluu96@gmail.com>
2022-07-23 00:07:26 -0700

    Did title

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -4,8 +4,8 @@ function Header() {
 	return (
 		<header className='jumbotron' style={{ backgroundColor: "#008080" }}>
 			<div className='container text-white'>
-				<h1 className='display-4'>Flashcard-o-matic</h1>
-				<p className='lead'>Discover The Flashcard Difference.</p>
+				<h1 className='display-4'>Finest Flashcards</h1>
+				<p className='lead'>The Way To Straight A's.</p>
 			</div>
 		</header>
 	);
